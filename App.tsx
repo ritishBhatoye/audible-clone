922894a06d126a6d7527e906d5ca823f04e6e253
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-04-13 18:01:52 +0530

    native-wind-setup

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import "./global.css";
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text classNae>Open up App.tsx to start working on your app!</Text>
       <Text> Hi </Text>
       <StatusBar style="auto" />
     </View>
